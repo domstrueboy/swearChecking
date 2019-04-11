9d2fec9d0b812f4daa981d317613b7295adf456e
p.bobylev <p.bobylev@info74.ru>
2019-04-11 18:24:37 +0500

    file reader added

--- swearChecking.go
+++ swearChecking.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	dictionaryFile, err := ioutil.ReadFile("data/txt/dictionary.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	// commentsFile, err := ioutil.ReadFile("data/txt/comments.txt")
+	// if err != nil {
+	// 	fmt.Print(err)
+	// }
+
+	dictionaryArr := strings.Split(string(dictionaryFile), "\n")
+	// comments := strings.Split(string(commentsFile), "\n")
+
+	// fmt.Println(dictionary)
+	// fmt.Println(comments)
+
+	// var dictionary map[string]bool{}
+
+	for i, word := range dictionaryArr {
+		fmt.Println(i, word)
+	}
 }
